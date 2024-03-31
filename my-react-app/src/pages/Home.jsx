247a3844152943f05f9f674ca064822ed7fe4000
Benjamin <ben@art.com>
2024-03-31 15:58:57 -0400

    react

--- my-react-app/src/pages/Home.jsx
+++ my-react-app/src/pages/Home.jsx
@@ -1,10 +1,34 @@
-export default function Home(){
-   return(
-      <>
-         <h1>Home</h1>
-         <p>Insert Live Transit Updates</p>
-         <p>Interactive Transit Maps</p>
-      </>
-   )
+import GoogleMapReact from 'google-map-react';
+import React from "react";
 
+
+const AnyReactComponent = ({ text }) => <div>{text}</div>;
+
+export default function SimpleMap(){
+  const defaultProps = {
+    center: {
+      lat: 10.99835602,
+      lng: 77.01502627
+    },
+    zoom: 11
+  };
+
+  return (
+    <>
+    <h1>Home</h1>
+    <div style={{ height: '100vh', width: '100%' }}>
+      <GoogleMapReact
+        bootstrapURLKeys={{ key: "" }}
+        defaultCenter={defaultProps.center}
+        defaultZoom={defaultProps.zoom}
+      >
+        <AnyReactComponent
+          lat={59.955413}
+          lng={30.337844}
+          text="My Marker"
+        />
+      </GoogleMapReact>
+    </div>
+    </>
+  );
 }
